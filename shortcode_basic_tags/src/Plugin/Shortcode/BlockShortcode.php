c10ef556ceaf8ef009606beb6c8567ff0f7bbf7b
Szabo Denes <szabo.denes@internode.hu>
2021-10-05 15:02:12 +0200

    Fix the __construct() for the BlockShortcode

--- shortcode_basic_tags/src/Plugin/Shortcode/BlockShortcode.php
+++ shortcode_basic_tags/src/Plugin/Shortcode/BlockShortcode.php
@@ -43,19 +43,19 @@ class BlockShortcode extends ShortcodeBase {
    *   The plugin ID for the plugin instance.
    * @param mixed $plugin_definition
    *   The plugin implementation definition.
-   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
-   *   The entity type manager.
    * @param \Drupal\Core\Render\RendererInterface $renderer
    *   The renderer service.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
    */
   public function __construct(
     array $configuration,
-    $plugin_id,
-    $plugin_definition,
-    EntityTypeManagerInterface $entity_type_manager,
-    RendererInterface $renderer
+          $plugin_id,
+          $plugin_definition,
+    RendererInterface $renderer,
+    EntityTypeManagerInterface $entity_type_manager
   ) {
-    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $renderer);
     $this->entityTypeManager = $entity_type_manager;
     $this->renderer = $renderer;
   }
@@ -68,8 +68,8 @@ class BlockShortcode extends ShortcodeBase {
       $configuration,
       $plugin_id,
       $plugin_definition,
-      $container->get('entity_type.manager'),
-      $container->get('renderer')
+      $container->get('renderer'),
+      $container->get('entity_type.manager')
     );
   }
 
